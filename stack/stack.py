6133f2a8420a98b5ed642c09b2268b4f982e593a
Buddy Agyin <agyin3@gmail.com>
2020-07-23 00:08:45 -0600

    complete stack challenges

--- stack/stack.py
+++ stack/stack.py
@@ -1,3 +1,5 @@
+from singly_linked_list import Node, LinkedList
+
 """
 A stack is a data structure whose primary purpose is to store and
 return elements in Last In First Out order. 
@@ -13,13 +15,37 @@ return elements in Last In First Out order.
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = LinkedList()
 
     def __len__(self):
-        pass
+        return self.size
 
     def push(self, value):
-        pass
+        self.storage.add_to_tail(value)
+        self.size = self.storage.length
 
     def pop(self):
-        pass
+        removed = self.storage.remove_tail()
+        self.size = self.storage.length
+        return removed
+
+####### ARRAY IMPLEMENTATION #######
+
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return self.size
+
+#     def push(self, value):
+#         self.size += 1
+#         self.storage.append(value)
+
+#     def pop(self):
+#         if self.size == 0:
+#             return None
+
+#         self.size -= 1
+#         return self.storage.pop()
